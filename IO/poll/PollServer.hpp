290f2d5c6aa81b7436145b74d701569f661a34ed
qiyu <qiyu@localhost.localdomain>
2019-07-30 15:45:29 +0800

    PollServer 可同时维护连接无上限（不同于select）

--- IO/poll/PollServer.hpp
+++ IO/poll/PollServer.hpp
@@ -3,13 +3,74 @@
 #include <vector>
 #include <errno.h>
 #include <cstring>
-#define MAX 128
 
 class PollServer
 {
 private:
     Socket *listen_sock;
     int port;
+    struct pollfd *fds = nullptr;
+    size_t fds_size = 2;
+private:
+    void Handle(nfds_t& nfds, int listen_sock_fd)
+    {
+        for(nfds_t i = 0; i < nfds; ++i)
+        {
+            if(fds[i].revents & POLLIN) //该描述符读事件就绪
+            {
+                if(fds[i].fd == listen_sock_fd) //有新的连接
+                {
+                    int fd = listen_sock->Accept();
+                    if(fd < 0)
+                        continue;
+                    if(nfds == fds_size) //fds需要扩容，poll可同时维护的连接无上限，因为可对fds扩容
+                    {
+                        fds_size *= 2;
+                        fds = (struct pollfd*)realloc(fds, sizeof(struct pollfd) * fds_size);
+                        cout << "fds expind." << endl;
+                    }
+                    fds[nfds].fd = fd;
+                    fds[nfds].events = POLLIN;
+                    fds[nfds].revents = 0;
+                    ++nfds;
+                    cout << "Have a new connection..." << endl;
+                }
+                else
+                {
+                    char buf[1024];
+                    ssize_t size = recv(fds[i].fd, buf, sizeof(buf), 0);
+                    if(size < 0)
+                    {
+                        cerr << "recv error." << endl;
+                    }
+                    else if(size == 0) //对端关闭连接
+                    {
+                        close(fds[i].fd);
+                        nfds_t j = 0;
+                        for(; j < nfds; ++j)
+                        {
+                            if(fds[j].fd == fds[i].fd)
+                            {
+                                break;
+                            }
+                        }
+                        while(j < nfds-1) //需确保fds数组内容连续
+                        {
+                            fds[j] = fds[j+1];
+                            ++j;
+                        }
+                        --nfds;
+                        --i; //因数组从i位置开始后半部分统一前移
+                        cout << "Client quit..." << endl;
+                    }
+                    else
+                    {
+                        cout << "Recv msg: " << buf << endl;
+                    }
+                }
+            }
+        }
+    }
 public:
     PollServer(int _port):port(_port)
     {}
@@ -24,86 +85,33 @@ public:
             cerr << "setsockopt failed." << endl;
         listen_sock->Bind();
         listen_sock->Listen();
+        fds = new struct pollfd[fds_size];
     }
 
     void Run()
     {
         int listen_sock_fd = listen_sock->GetSocket();
-        struct pollfd *fds = new struct pollfd[32];
         fds[0].fd = listen_sock_fd;
         fds[0].events = POLLIN;
         fds[0].revents = 0;
+        nfds_t nfds = 1;
         while(1)
         {
-            int max_fd = 0;
-            for(size_t i = 0; i < MAX; ++i) //设置readfds,即告诉系统我关心哪些文件的读就绪事件
-            {
-                if(fdset[i] == -1)
-                    continue;
-                FD_SET(fdset[i], &readfds);
-                max_fd = fdset[i] > max_fd ? fdset[i] : max_fd; 
-            }
-            int n = select(max_fd+1, &readfds, nullptr, nullptr, nullptr); //timeval为nullptr，select阻塞式的等待事件就绪
-            //cout << "n = " << n << endl;
+            int n = poll(fds, nfds, -1);
             if(n < 0)
             {
-                cerr << "select error." << endl;
+                cerr << "poll error." << endl;
                 continue;
             }
-            for(size_t i = 0; i < MAX; ++i) 
+            else if(n == 0)
             {
-                if(fdset[i] == -1)
-                    continue;
-                if(FD_ISSET(fdset[i], &readfds))
-                {
-                    if(fdset[i] == listen_sock_fd) //有新的连接建立请求
-                    {
-                        int fd = listen_sock->Accept();
-                        if(fd < 0) //Accept可能失败
-                            continue;
-                        size_t j = 0;
-                        while(j < MAX && fdset[j] != -1) ++j;
-
-                        if(j == MAX)
-                        {
-                            close(fd); //若描述符集合已满，即服务器已无法接受新的连接请求
-                            cout << "Server full load..." << endl;
-                        }
-                        else
-                        {
-                            fdset[j] = fd; //将新连接描述符加入集合，下次select去检查
-                            cout << "Have a new connection..." << endl;
-                            // for(auto it : fdset)
-                            // {
-                            //     cout << it << " ";
-                            // }
-                            // cout << endl;
-                        }
-                    }
-                    else //已建立的连接有读事件就绪
-                    {
-                        char buf[1024] = {0};
-                        ssize_t size = recv(fdset[i], buf, sizeof(buf)-1, 0);
-                        if(size < 0)
-                        {
-                            cerr << "recv error." << endl;
-                            continue;
-                        }
-                        else if(size == 0) //对端关闭了连接
-                        {
-                            close(fdset[i]);
-                            cout << "Have a connection closed..." << endl;
-                            fdset[i] = -1; //在描述符集合中删除该连接，下次select时不再检查
-                            // for(auto it : fdset)
-                            // {
-                            //     cout << it << " ";
-                            // }
-                            // cout << endl;
-                        }
-                        else //size > 0，读到了发来的数据
-                            cout << "Client: " << buf << endl; 
-                    }
-                }
+                cout << "timeout..." << endl;
+                continue;
+            }
+            else //有事件就绪
+            {
+                cout << "Handle..." << endl;
+                Handle(nfds, listen_sock_fd);
             }
         }
     }
---
+++ IO/poll/Socket.hpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+using namespace std;
+#define BACKLOG 5
+
+class Socket
+{
+private:
+    int sock;
+    int port;
+public:
+    Socket(int _port):sock(-1), port(_port)
+    {}
+    void Create()
+    {
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if(sock < 0)
+        {
+            cerr << "create socket failed." << endl;
+            exit(1);
+        }
+    }
+    void Bind()
+    {
+        sockaddr_in address;
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = htonl(INADDR_ANY);
+        address.sin_port = htons(port);
+        if(bind(sock, (sockaddr *)&address, sizeof(address)) != 0)
+        {
+            cerr << "bind failed." << endl;
+            exit(2);            
+        }
+    }
+    void Listen()
+    {
+        if(listen(sock, BACKLOG) != 0)
+        {
+            cerr << "listen failed." << endl;
+            exit(3);
+        }
+    }
+    int Accept()
+    {
+        sockaddr peer;
+        socklen_t len = sizeof(peer);
+        int fd = accept(sock, (sockaddr *)&peer, &len);
+        if(fd < 0)
+        {
+            cerr << "accept failed." << endl;
+        }
+        return fd;
+    }
+    int GetSocket()
+    {
+        return sock;
+    }
+    ~Socket()
+    {
+        if(sock > 0)
+        {
+            close(sock);
+        }
+    }
+};
